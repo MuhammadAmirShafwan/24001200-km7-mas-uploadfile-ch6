8e8f88d18713ba3cf471c1b097c4b32ab8fc7360
Muhammad Amir Shafwan <127172857+MuhammadAmirShafwan@users.noreply.github.com>
2024-11-08 22:34:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 const morgan = require("morgan");
 const router = require("./routes");
 
