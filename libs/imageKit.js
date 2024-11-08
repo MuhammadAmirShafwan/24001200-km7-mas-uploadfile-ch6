632d9a08444daef513991c5763fd9fcb852494c6
MuhammadAmirShafwan <muhammadamirshafwan69@gmail.com>
2024-11-08 22:32:06 +0700

    Reset remote repository

--- libs/imageKit.js
+++
@@ -1,9 +0,0 @@
-const ImageKit = require('imagekit');
-
-const imagekit = new ImageKit({
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENPOINT
-});
-
-module.exports = imagekit
